70c0eab62e6d2118e3485892c4c7595c510a2d5f
Fasina Oreoluwa <dannyoma75@gmail.com>
2022-05-21 06:32:45 +0100

    + added notification modals with their states

--- pages/edit-article.tsx
+++ pages/edit-article.tsx
@@ -24,6 +24,9 @@ export default function AddArticle() {
   const [DOIURL, setDOIURL] = useState('');
   const { status, user } = useContext<any>(AuthContext);
   const [_id,setId] = useState('');
+  const [showErrorModal, setErrorModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showSuccessModal, setSuccessModal] = useState(false);
   useEffect(() => {
       console.log(query);   
     console.log({ KI: status.loggedIn, user: user.token })
@@ -34,6 +37,14 @@ export default function AddArticle() {
       'Authorization': `Bearer ${JSON.parse(window.localStorage.getItem('TOKEN'))}`
     }})
     console.log(res);
+
+    if(res.data.success){
+      setSuccessModal(true)
+     }
+     else {
+       setErrorMessage(res.data.message);
+       setErrorModal(true);
+     }
   };
   const handleGetArticle = async () => {
     const res = await axios.get(`${API}/article/get/${query.id}`,{headers: {
@@ -61,6 +72,25 @@ export default function AddArticle() {
   return (
 
     <div>
+         {showSuccessModal ? (
+            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+              <strong className="font-bold">Article Update Success !</strong>
+              <span className="block sm:inline">{title} was added successfully to your publications.</span>
+              <span onClick = {() => setSuccessModal(false)} className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg className="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+              </span>
+          </div>
+          ) :  null}
+        {showErrorModal ? (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong className="font-bold">Update Error !</strong>
+                <span className="block sm:inline">{title} wasn't updated successfully. {errorMessage}</span>
+                <span onClick = {() => setErrorModal(false)} className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                  <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+            </div>
+
+        ) : null}
         <section className='w-full p-9 mx-18 flex justify-center'>
           <form className='border p-5 form'>
             <h1 className='text-4xl tracking-wide	font-medium my-9'>Edit article</h1>
